90a52b71186a434a18a9357f8c1fa68189d35691
ALOUAN Mouad <marocoujdamouad@gmail.com>
2021-03-28 23:01:10 +0100

    Profile update & home page

--- src/main/java/org/application/service/RecruteurService.java
+++ src/main/java/org/application/service/RecruteurService.java
@@ -8,6 +8,7 @@ import org.application.entities.Recruteur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,36 +19,14 @@ public class RecruteurService {
 	RecruteurRepository recruteurRepository;
 	
 	
-	public void CreateRecruiter(Recruteur r)
+	
+	public Recruteur modifierRecruteur(@RequestParam("id") long id,String Recruteur_firstname,String Recruteur_lastname)
 	{
-			
-		
-			
-			/*
-			r.setCompany_name(company_name);
-			r.setCompany_speciality(company_speciality);
-			r.setCompany_speciality(company_speciality);
-			r.setCompany_url(company_url);
-			r.setRecruteur_phone(recruteur_phone);
-			r.setRecruteur_firstname(recruteur_firstname);
-			r.setRecruteur_lastname(recruteur_lastname);
-			/*r.setIdUser(id_user);*/
-			
-			recruteurRepository.save(r);
-			
+		Recruteur recFromDb = recruteurRepository.findById(id).get();
+		recFromDb.setRecruteur_firstname(Recruteur_firstname);
+		recFromDb.setRecruteur_firstname(Recruteur_lastname);
+		recruteurRepository.save(recFromDb);
+		return recFromDb;
 	}
-		public void SaveProfilePicture(@ModelAttribute MultipartFile file)
-		{
-			String fileName= org.springframework.util.StringUtils.cleanPath(file.getOriginalFilename());
-			if(fileName.contains(".."))
-			{
-				System.out.println("File not valide");
-			}
-			//try {
-				//recruteurRepository.setPhoto(Base64.getEncoder().encodeToString(file.getBytes()));
-			//}
-			//catch(IOException e){ e.printStackTrace();}
-			
-		}
 		
 }
--- src/main/java/org/application/web/RecruiterController.java
+++ src/main/java/org/application/web/RecruiterController.java
@@ -6,19 +6,24 @@ import java.util.Base64;
 
 import org.application.Repository.RecruteurRepository;
 import org.application.entities.Recruteur;
+import org.application.service.RecruteurService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+@Component
+
 @Controller
 public class RecruiterController {
 	@Autowired
 	private RecruteurRepository recruiterRepository;
-	
+	private RecruteurService service;
 	
 	
 	
@@ -41,7 +46,6 @@ public class RecruiterController {
 		
 		r.setCompany_name(company_name);
 		r.setCompany_speciality(company_speciality);
-		//r.setCompany_speciality(company_speciality);
 		r.setCompany_url(company_url);
 		r.setRecruteur_phone(recruteur_phone);
 		r.setRecruteur_firstname(recruteur_firstname);
@@ -49,12 +53,44 @@ public class RecruiterController {
 		//r.setIdUser(id_user);
 		
 		recruiterRepository.save(r);
-		//return "recruiter/index";
 		ModelAndView mav = new ModelAndView("recruteur/MonProfile");
         mav.addObject("recruteur", r);
         return mav;
 	}
-		
 	
+/*	@GetMapping("/profile")
+	public ModelAndView getProfile(@RequestParam("id")  Long id)
+	{
+		Recruteur recFromDb = recruiterRepository.findById(id).get();
+		ModelAndView mav = new ModelAndView("recruteur/MonProfile");
+        mav.addObject("recruteur", recFromDb);
+        return mav;
+	}
+*/	
+	@GetMapping("/modifierForm")
+	public ModelAndView getProfileForm(@RequestParam("id")  Long id)
+	{
+		Recruteur recFromDb = recruiterRepository.findById(id).get();
+		ModelAndView mav = new ModelAndView("recruteur/upProfile");
+        mav.addObject("recruteur", recFromDb);
+        return mav;
+	}
+	
+    @Autowired
+	RecruteurRepository recruteurRepository ;
+	@PostMapping("/modifier")
+	public ModelAndView modifier(@RequestParam("id") Long id,  String recruteur_firstname,String recruteur_lastname,String recruteur_phone, String company_url)
+	{
+		Recruteur recFromDb = recruteurRepository.findById(id).get();
+		recFromDb.setRecruteur_firstname(recruteur_firstname);
+		recFromDb.setRecruteur_lastname(recruteur_lastname);
+		recFromDb.setRecruteur_phone(recruteur_phone);
+		recFromDb.setCompany_url(company_url);
+		Recruteur r =recruteurRepository.save(recFromDb);
+		//Recruteur r=service.modifierRecruteur(id, recruteur_firstname,recruteur_lastname);
+		ModelAndView mav = new ModelAndView("recruteur/MonProfile");
+        mav.addObject("recruteur", r);
+        return mav;
+	}
 	
 }
